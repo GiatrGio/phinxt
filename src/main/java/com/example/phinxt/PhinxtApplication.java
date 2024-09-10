b2e18e17fc50d63e40b90d2c06c22c5d6d13d8e9
giatrgio <georgegiatrakis@gmail.com>
2024-09-10 20:46:09 +0200

    First commit

---
+++ src/main/java/com/example/phinxt/PhinxtApplication.java
@@ -0,0 +1,13 @@
+package com.example.phinxt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhinxtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhinxtApplication.class, args);
+    }
+
+}
